9bacb40221158f32a38a68ce0d1ede970c2b9c95
sivasai <sivasai3920@gmail.com>
2023-01-27 18:58:58 +0530

    Smooth Scrolling using JS

--- js/script.js
+++ js/script.js
@@ -17,12 +17,40 @@ const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
 ////////////////////////////////////////
-
+//closin and opening of nav-button
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
 
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
+});
+
+///////////////////////////////////////////////////////
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    //scroll back to top
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    //Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    //Close mobile Navigation
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
 });
 
 ///////////////////////////////////////////////////////////
